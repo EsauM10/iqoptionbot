65c3043c6899dc881cb24a6ea2c96904bab1fea1
EsauM10 <esau2096@gmail.com>
2023-12-25 18:02:22 -0300

    feat: add settings page html

--- static/js/dashboard/settings.js
+++ static/js/dashboard/settings.js
@@ -17,7 +17,44 @@ class SettingsPage {
     render() {
         document.getElementById("openAssets").classList.add("hidden")
         document.getElementById("page").innerHTML = `
-            <div><h1>Settings</h1></div>
+            <div class="settings-page">
+                <form>
+                    <div class="input-wrapper">
+                        <label>Conta</label>    
+                        <label>
+                            <input type="radio" name="account-type" value="PRACTICE" checked="true">
+                            Treinamento
+                        </label>
+                    </div>
+                    <div class="input-wrapper">
+                        <label>
+                            <input type="radio" name="account-type" value="REAL">
+                            Real
+                        </label>
+                    </div>
+                    <div>
+                        <label>Valor da entrada</label>
+                        <input type="number" value="1.0" step="0.1" required="true" min="1.0">
+                    </div>
+                    <span></span>
+                    <div>
+                        <label>Stop Win</label>
+                        <input type="number" value="1.0" step="0.1" required="true" min="1.0">
+                    </div>
+                    <div>
+                        <label>Stop Loss</label>
+                        <input type="number" value="1.0" step="0.1" required="true" min="1.0">
+                    </div>
+                    <div>
+                        <label>Martingale</label>
+                        <input type="number" value="0" step="1" required="true" min="0">
+                    </div>
+                    <div>
+                        <label>Soros</label>
+                        <input type="number" value="0" step="1" required="true" min="0">
+                    </div>
+                </form>
+            </div>
         `
     }
 }
